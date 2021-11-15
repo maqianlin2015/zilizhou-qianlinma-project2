4137fa60896697b38be45ed239290cddcb7a7d5f
zili zhou <zilizzhou@gmail.com>
2021-11-14 23:15:25 -0800

    updates

--- src/components/NewGame.jsx
+++ src/components/NewGame.jsx
@@ -21,13 +21,13 @@ const NewGame = ({ setAppStatus, setWinner }) => {
 			{/* <button className="new-game-btn star-btn" onClick={onStartGame}> */
 			/*添加了div classname buttons和对应的intro.css style*/}
 			<div className="buttons">
-			<button className="" onClick={onStartNormalGame}>
+			<button className="normal-game-btn" onClick={onStartNormalGame}>
 				Normal Game
 			</button>
 			{/*add space between two buttons*/}
 			<div id="divider" />
 			{/* <button className="start-free-player-game-btn star-btn" onClick={onStartGame}> */}
-			<button className="" onClick={onStartFreePlayerGame}>
+			<button className="free-player-game-btn" onClick={onStartFreePlayerGame}>
 				{/* free player btn 还没有对应的comp+func，写完了normal回来改 */}
 				Free Player Game
 			</button>
